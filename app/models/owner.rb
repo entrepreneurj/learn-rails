1b355ab230d584129098373d2812cf322e2676db
Joe <entrepreneurj@gmail.com>
2013-11-30 17:50:39 +0000

    Rails loggery

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -9,6 +9,7 @@ class Owner
   end
 
   def countdown
+    Rails.logger.debus 'DEBUG: entering Owner countdown method'
     today = Date.today
     birthday = Date.new(today.year, birthdate.month, birthdate.day)
     if birthday > today
